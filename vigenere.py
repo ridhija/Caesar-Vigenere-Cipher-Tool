826ac21d75a3170c036dda1db4f5df9dfa951b37
ridhija <153988252+ridhija@users.noreply.github.com>
2026-09-19 15:40:42 +0530

    Add Vigenere cipher implementation

---
+++ vigenere.py
@@ -0,0 +1,30 @@
+def vigenere_encrypt(text, key):
+    result = ""
+    key = key.upper()
+    key_index = 0
+
+    for char in text:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - ord('A')
+            result += chr((ord(char.upper()) - ord('A') + shift) % 26 + ord('A'))
+            key_index += 1
+        else:
+            result += char
+
+    return result
+
+
+def vigenere_decrypt(text, key):
+    result = ""
+    key = key.upper()
+    key_index = 0
+
+    for char in text:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - ord('A')
+            result += chr((ord(char.upper()) - ord('A') - shift) % 26 + ord('A'))
+            key_index += 1
+        else:
+            result += char
+
+    return result
